818769a1be95ca3e8a12503e7d39aea2b13d422b
binf1611@gmail.com <fpawel>
2019-12-24 16:03:44 +0300

    bsrc.bat

---
+++ bsrc.bat
@@ -0,0 +1,3 @@
+git clone https://github.com/fpawel/atool build\atoolsrc\atool
+git clone https://github.com/fpawel/atoolgui build\atoolsrc\atoolgui
+7z a -tzip -sdel build\atoolsrc.zip ".\build\atoolsrc"
