abe2d867d539fc9185888851bc92ac06d86ac283
binf1611@gmail.com <fpawel>
2020-01-22 10:08:56 +0300

    lua client

--- cmd/lua/main.go
+++ cmd/lua/main.go
@@ -28,31 +28,33 @@ func main() {
 		panic(err)
 	}
 
-	//transportFactory := thrift.NewTTransportFactory()
-	//transport, err = transportFactory.GetTransport(transport)
-	//if err != nil {
-	//	panic(err)
-	//}
+	protocolFactory := thrift.NewTBinaryProtocolFactoryDefault()
+	transportFactory := thrift.NewTTransportFactory()
+
+	transport, err = transportFactory.GetTransport(transport)
+	if err != nil {
+		panic(err)
+	}
 
 	if err := transport.Open(); err != nil {
 		panic(err)
 	}
-	defer func() {
-		log.ErrIfFail(transport.Close)
-	}()
+	defer transport.Close()
 
-	protocolFactory := thrift.NewTBinaryProtocolFactoryDefault()
-	iProto := protocolFactory.GetProtocol(transport)
-	oProto := protocolFactory.GetProtocol(transport)
+	iProto := thrift.NewTMultiplexedProtocol(protocolFactory.GetProtocol(transport), "ScriptService")
+	oProto := thrift.NewTMultiplexedProtocol(protocolFactory.GetProtocol(transport), "ScriptService")
 
 	client := api.NewScriptServiceClient(thrift.NewTStandardClient(iProto, oProto))
 
 	if len(os.Args) > 1 && os.Args[1] == "run" {
 		filename := strings.Join(os.Args[2:], "")
-		if err := client.RunFile(context.Background(), filename); err != nil {
+		if err := client.RunFile(defaultCtx, filename); err != nil {
 			log.PrintErr(err)
 		}
 	}
 }
 
-var log = structlog.New()
+var (
+	log        = structlog.New()
+	defaultCtx = context.Background()
+)
