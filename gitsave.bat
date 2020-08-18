104a1e5891c43af3994081fd272637a23ae05a7e
binf1611@gmail.com <fpawel>
2020-08-18 13:49:15 +0300

    save

--- gitsave.bat
+++ gitsave.bat
@@ -1,3 +1,6 @@
 git add .
 git commit -m "save"
 git push origin master
+SET GUIREPO = --git-dir=%DELPHIPATH%\src\github.com\fpawel\aToolGui\.git
+git GUIREPO commit -m "save"
+git GUIREPO push origin master
